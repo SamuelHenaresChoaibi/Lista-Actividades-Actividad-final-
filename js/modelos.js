bebc520fe818bc94dcf0aa58e422f680a560686e
Samuel Henares Choaibi <samuelhenares@paucasesnovescifp.cat>
2025-04-24 23:48:52 +0200

    Commit: cambio en los nombres de las variables y parámetros del módulo 'modelos.js' y archivos JSON añadidos

--- js/modelos.js
+++ js/modelos.js
@@ -1,22 +1,22 @@
 class Task {
-    constructor(id, titulo, descripcion, fecha, categoria, prioridad, realizada) {
+    constructor(id, titulo, descripcio, data, categoria, prioritat, realitzada) {
         this.id = id;
         this.titulo = titulo;
-        this.descripcion = descripcion;
-        this.fecha = fecha;
+        this.descripcio = descripcio;
+        this.data = data;
         this.categoria = categoria;
-        this.prioridad = prioridad;
-        this.realizada = realizada;
+        this.prioritat = prioritat;
+        this.realitzada = realitzada;
     }
     
     marcarComoRealizada() {
-        this.realizada = true;
+        this.realitzada = true;
     }
 }
 
-class Category{
-    constructor(nombre, color){
-        this.nombre = nombre;
+class Category {
+    constructor(nom, color){
+        this.nom = nom;
         this.color = color;
     }
 }
