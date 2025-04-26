0e586ba46a057fc0b669f55954d30f2033b74fa6
Samuel Henares Choaibi <samuelhenares@paucasesnovescifp.cat>
2025-04-26 20:19:35 +0200

    Commit form-tasca.js
    Módulo form-tasca.js - FINALIZADO
    
    Me he basado en el módulo de categoría, donde funciona muy parecido y casi prácticamente igual. Si hay algún cambio, se irá modificando en el 'main'.

---
+++ js/form-tasca.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const nomInput = document.getElementById('name-task');
+    const descripcionInput = document.getElementById('description');
+    const fechaInput = document.getElementById('date-task');
+    const categoriaSelect = document.getElementById('categoria');
+    const prioridadSelect = document.getElementById('priority');
+    const botonCrearTasca = document.getElementById('create-task');
+    const categorias = cargarCategorias();
+
+    function cargarCategoriasSelect() {
+        categoriaSelect.innerHTML = '';
+        categorias.forEach(categoria => {
+            const option = document.createElement('option');
+            option.value = categoria.nom;
+            option.textContent = categoria.nom;
+            categoriaSelect.appendChild(option);
+        });
+    }
+
+    botonCrearTasca.addEventListener('click', () => {
+        const nomValue = nomInput.value.trim();
+        const descripcionValue = descripcionInput.value.trim();
+        const fechaValue = fechaInput.value;
+        const categoriaValue = categoriaSelect.value;
+        const prioridadValue = prioridadSelect.value;
+
+        if (!nomValue || !descripcionValue || !fechaValue || !categoriaValue || !prioridadValue) {
+            alert("Hay que rellenar todos los campos para crear una tarea");
+        } else {
+            const tareas = cargarTareas();
+            const tareaNueva = new Tarea(
+                generarIdTarea(tareas), 
+                nomValue, 
+                descripcionValue, 
+                fechaValue, 
+                categoriaValue, 
+                prioridadValue, 
+                false);
+
+            tareas.push(tareaNueva);
+            guardarTareas(tareas);
+            nomInput.value = '';
+            descripcionInput.value = '';
+            fechaInput.value = '';
+            prioridadSelect.value = 'alta';
+        }
+    });
+
+    cargarCategoriasSelect();
+});
